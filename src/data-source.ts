9148554261b3e541f82452c4be1016304372ba74
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-15 12:52:14 -0500

    Testing DataSourse url

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,16 +2,27 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
+
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "admin",
-    password: "root",
-    database: "collar-club",
+    url: process.env.DATABASE_URL,
     synchronize: true,
     logging: true,
     entities: [User],
     migrations: [],
     subscribers: [],
-})
+});
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     host: "localhost",
+//     port: 5432,
+//     username: "admin",
+//     password: "root",
+//     database: "collar-club",
+//     synchronize: true,
+//     logging: true,
+//     entities: [User],
+//     migrations: [],
+//     subscribers: [],
+// })
