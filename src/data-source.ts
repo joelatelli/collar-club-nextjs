f0525567edabd8c67a239e7f4236c6af8889df52
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-15 13:38:33 -0500

    Testing new DataSource config

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,11 @@ require('dotenv').config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DATABASE_URL,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: Number(process.env.PROD_DB_PORT || "5432"),
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
     synchronize: true,
     logging: true,
     entities: [User],
