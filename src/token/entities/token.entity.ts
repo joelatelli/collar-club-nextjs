57b6a38854f9e602ae9110619f3620944323e944
Joel Muflam <joel@juniperconsultinggroup.com>
2024-02-20 21:25:17 -0500

    Adding token folder

---
+++ src/token/entities/token.entity.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, Index, ManyToOne, JoinColumn } from "typeorm";
+import { BaseEntity } from "../../libs";
+import { CustomerEntity } from "../../customer";
+
+@Entity({ name: "tokens" })
+export class TokenEntity extends BaseEntity {
+
+    @Index()
+    @Column()
+    accessToken!: string;
+
+    @Column()
+    refreshToken!: string;
+
+    @ManyToOne(() => CustomerEntity)
+    @JoinColumn({ name: "userId" })
+    customer!: CustomerEntity;
+}
